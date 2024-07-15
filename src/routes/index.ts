f814fefaad32a9de0daed1a5d21c0be5f1218350
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-07-15 13:57:41 +0200

    first commit

---
+++ src/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import TodosRouter from './todos';
+import UserRouter from './users';
+
+const AppRouter = Router();
+
+AppRouter.use('/todos', TodosRouter);
+AppRouter.use('/users', UserRouter);
+
+AppRouter.get('/', (req, res) => {
+  res.status(200).send('Hello, World!');
+});
+
+export default AppRouter;

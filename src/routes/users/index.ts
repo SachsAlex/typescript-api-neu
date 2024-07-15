d678e3cccdaa24a244eea53b0d0e6fc85bb5ac05
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-07-15 15:07:53 +0200

    update prettier

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,11 +1,11 @@
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import UserModel from "../../database/models/UserModel";
-import { Router } from "express";
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import UserModel from '../../database/models/UserModel';
+import { Router } from 'express';
 
 const UserRouter = Router();
 
 // GET REQUESTS
-UserRouter.get("/currentuser", async (req, res) => {
+UserRouter.get('/currentuser', async (req, res) => {
   const userId = parseInt(req.query.id as string);
 
   const user = await UserModel.findOne({ where: { id: userId } });
